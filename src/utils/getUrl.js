13df557ee47b518b8a2a57ee5bfbf356e42063ae
ProgettoGPOI5e <65616761+ProgettoGPOI5e@users.noreply.github.com>
2020-05-19 19:49:35 +0200

    Update getUrl.js

--- src/utils/getUrl.js
+++ src/utils/getUrl.js
@@ -2,6 +2,6 @@ export default (env) => {
   if (env !== 'production') {
     return process.env.VUE_APP_LOCAL || 'http://localhost:3000'
   } else {
-    return 'https://cobos-api.herokuapp.com' || process.env.API_URI || window.location.origin
+    return 'https://progettogpoi5e-api.herokuapp.com' || process.env.API_URI || window.location.origin
   }
 }
